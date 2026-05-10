34073d46f7a7eb1da36bb4e9e5243f2c326a9444
Jahnvi_777 <148742397+Jahnvi777-git@users.noreply.github.com>
2026-05-11 00:45:43 +0530

    captian route

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -2,6 +2,7 @@ const { hash } = require('bcrypt');
 const captainModel = require('../models/captain.model');
 const { validationResult } = require('express-validator');
 const captainService = require('../services/captain.service');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 
 
 module.exports.registerCaptain = async (req, res, next) => {
@@ -28,4 +29,35 @@ module.exports.registerCaptain = async (req, res, next) => {
     });
     const token = captain.generateAuthToken();
     res.status(201).json({ token, captain });
+}
+
+module.exports.loginCaptain = async (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    const captain = await captainModel.findOne({ email });
+    if(!captain) {
+        return res.status(400).json({ message: 'Invalid email or password' });
+    }
+    const isMatch = await captain.comparePassword(password);
+    if(!isMatch) {
+        return res.status(400).json({ message: 'Invalid email or password' });
+    }
+    const token = captain.generateAuthToken();
+    res.cookie('token', token);
+    res.status(200).json({ token, captain });
+}
+
+module.exports.getCaptainProfile = async (req, res, next) => {
+    const captain = req.captain;
+    res.status(200).json({ captain });
+}
+
+module.exports.logoutCaptain = async (req, res, next) => {
+    const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
+    await blacklistTokenModel.create({ token });
+    res.clearCookie('token');
+    res.status(200).json({ message: 'Logged out successfully' });
 }
