86ab1b2d67229d4769f714573860d98c0831d2ba
Jahnvi_777 <148742397+Jahnvi777-git@users.noreply.github.com>
2026-06-19 01:29:27 +0530

    search panel for locations

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+      <div className='flex items-center gap-4 my-4 justify-start'>
+        <h2 className='bg-[#eee] p-2 h-10 w-12 flex items-center justify-center rounded-full'>
+          <i className='ri-map-pin-fill text-xl'></i>
+        </h2>
+        <h4 className='font-medium'>24B, near Kapoor's cafe, near abc school, hyderabad</h4>
+      </div>
+      <div className='flex items-center gap-4 my-4 justify-start'>
+        <h2 className='bg-[#eee] p-2 h-10 w-12 flex items-center justify-center rounded-full'>
+          <i className='ri-map-pin-fill text-xl'></i>
+        </h2>
+        <h4 className='font-medium'>24B, near Kapoor's cafe, near abc school, hyderabad</h4>
+      </div>
+      <div className='flex items-center gap-4 my-4 justify-start'>
+        <h2 className='bg-[#eee] p-2 h-10 w-12 flex items-center justify-center rounded-full'>
+          <i className='ri-map-pin-fill text-xl'></i>
+        </h2>
+        <h4 className='font-medium'>24B, near Kapoor's cafe, near abc school, hyderabad</h4>
+      </div>
+      <div className='flex items-center gap-4 my-4 justify-start'>
+        <h2 className='bg-[#eee] p-2 h-10 w-12 flex items-center justify-center rounded-full'>
+          <i className='ri-map-pin-fill text-xl'></i>
+        </h2>
+        <h4 className='font-medium'>24B, near Kapoor's cafe, near abc school, hyderabad</h4>
+      </div>
+      <div className='flex items-center gap-4 my-4 justify-start'>
+        <h2 className='bg-[#eee] p-2 h-10 w-12 flex items-center justify-center rounded-full'>
+          <i className='ri-map-pin-fill text-xl'></i>
+        </h2>
+        <h4 className='font-medium'>24B, near Kapoor's cafe, near abc school, hyderabad</h4>
+      </div>
+      <div className='flex items-center gap-4 my-4 justify-start'>
+        <h2 className='bg-[#eee] p-2 h-10 w-12 flex items-center justify-center rounded-full'>
+          <i className='ri-map-pin-fill text-xl'></i>
+        </h2>
+        <h4 className='font-medium'>24B, near Kapoor's cafe, near abc school, hyderabad</h4>
+      </div>
+      <div className='flex items-center gap-4 my-4 justify-start'>
+        <h2 className='bg-[#eee] p-2 h-10 w-12 flex items-center justify-center rounded-full'>
+          <i className='ri-map-pin-fill text-xl'></i>
+        </h2>
+        <h4 className='font-medium'>24B, near Kapoor's cafe, near abc school, hyderabad</h4>
+      </div>
+      
+    </div>
+  )
+}
+
+export default LocationSearchPanel
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react'
 import {useGSAP} from '@gsap/react'
 import gsap from 'gsap'
 import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from '../components/LocationSearchPanel'
 
 const Home = () => {
   const [pickup, setPickup] = React.useState('')
@@ -17,23 +18,23 @@ const Home = () => {
 
   useGSAP(() => {
     if(panelOpen) {
-      gsap.to(panelRef.current, {height: '75%', opacity: 1})
+      gsap.to(panelRef.current, {height: '75%', opacity: 1, padding: 24})
       gsap.to(panelCloseRef.current, {opacity: 1})
     }
     else {
-      gsap.to(panelRef.current, {height: '0'})
+      gsap.to(panelRef.current, {height: '0', opacity: 0, padding: 0})
       gsap.to(panelCloseRef.current, {opacity: 0})
     }
   }, [panelOpen])
 
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
       <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/1920px-Uber_logo_2018.svg.png" alt='uber logo' />
       <div className='h-screen w-screen'>
         <img className='h-full w-full object-cover' src="https://www.medianama.com/wp-content/uploads/2018/06/Screenshot_20180619-112715.png.png" alt="" />
       </div>
-      <div className='flex flex-col justify-end h-full w-ful absolute top-0 w-screen rounded-lg'>
-        <div className='bg-white p-5 w-screen h-[25%] relative'>
+      <div className='flex flex-col justify-end h-full w-ful absolute top-0 w-screen rounded-lg overflow-hidden'>
+        <div className='bg-white p-5 w-screen h-fit relative'>
           <div>
             <h5 ref={panelCloseRef} className='text-xl absolute top-7 right-7 opacity-0' onClick={() => setPanelOpen(false)}>
               <i className='ri-arrow-down-wide-line'></i>
@@ -41,7 +42,7 @@ const Home = () => {
           </div>
           <h4 className='text-2xl font-semibold pt-2'>Find a Trip</h4>
           <form onSubmit={(e) => submitHandler(e)}>
-            <div className="line absolute h-[32%] w-1 top-[45%] bg-gray-900 left-10 rounded-full"></div>
+            <div className="line absolute h-[35%] w-1 top-[47%] bg-gray-900 left-10 rounded-full"></div>
             <input
               className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mb-5 mt-5' 
               type="text" 
@@ -60,8 +61,8 @@ const Home = () => {
             />
           </form>
         </div>
-        <div className='bg-white h-[0]' ref={panelRef}>
-
+        <div className='bg-white h-[0] overflow-hidden' ref={panelRef}>
+          <LocationSearchPanel />
         </div>
       </div>
     </div>
